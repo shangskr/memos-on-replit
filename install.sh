835d7f6154d89d867663e42de5d08e3dd326a954
王九弦SZ·Ninty <120767492+SinzMise@users.noreply.github.com>
2023-07-31 16:13:29 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ echo "GitHub开源地址：https://github.com/SinzMise/memos-on-replit"
 nix-env -iA nixpkgs.wget
 mkdir build
 cd build
-wget -O .replit https://github.com/SinzMise/memos-on-replit/raw/master/.replit
-wget -O replit.nix https://github.com/SinzMise/memos-on-replit/raw/master/replit.nix
+wget -O .replit https://github.com/SinzMise/memos-on-replit/raw/main/.replit
+wget -O replit.nix https://github.com/SinzMise/memos-on-replit/raw/main/replit.nix
 cd ..
-wget -O main.sh https://github.com/SinzMise/memos-on-replit/raw/master/main.sh
-wget -O memos https://github.com/SinzMise/memos-on-replit/raw/master/memos.moe
+wget -O main.sh https://github.com/SinzMise/memos-on-replit/raw/main/main.sh
+wget -O memos https://github.com/SinzMise/memos-on-replit/raw/main/memos.moe
 cp -r build/.replit . && cp -r build/replit.nix .
 echo "恭喜搭建完成"
 echo "点击Run立即运行项目"
