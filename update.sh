31be04f4015e367b5b8ff4cb9b19d7b90ccc2f95
安小歪 <119773750+shangskr@users.noreply.github.com>
2023-08-05 20:40:24 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 echo "Memos一键更新脚本"
-echo "脚本作者：王九弦SZ·Ninty"
-echo "GitHub开源地址：https://github.com/SinzMise/memos-on-replit"
-rm -rf memos && wget -O memos https://github.com/SinzMise/memos-on-replit/raw/main/memos.moe
+echo "脚本作者：安小歪"
+echo "GitHub开源地址：https://github.com/shangskr/memos-on-replit"
+rm -rf memos && wget -O memos https://github.com/shangskr/memos-on-replit/raw/main/memos.moe
 echo "更新至最新版本，更新完毕！"
 echo "点击Run按钮重新启动本项目！！！"
