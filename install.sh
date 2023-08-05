2de4f2fbc9c1f0f9fd722f64394df1c9aa3cea33
安小歪 <119773750+shangskr@users.noreply.github.com>
2023-08-05 20:42:26 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 echo "Memos一键搭建脚本"
-echo "脚本作者：王九弦SZ·Ninty"
-echo "GitHub开源地址：https://github.com/SinzMise/memos-on-replit"
+echo "脚本作者：安小歪"
+echo "GitHub开源地址：https://github.com/shangskr/memos-on-replit"
 nix-env -iA nixpkgs.wget
 mkdir build
 cd build
-wget -O .replit https://github.com/SinzMise/memos-on-replit/raw/main/.replit
-wget -O replit.nix https://github.com/SinzMise/memos-on-replit/raw/main/replit.nix
+wget -O .replit https://github.com/shangskr/memos-on-replit/raw/main/.replit
+wget -O replit.nix https://github.com/shangskr/memos-on-replit/raw/main/replit.nix
 cd ..
-wget -O main.sh https://github.com/SinzMise/memos-on-replit/raw/main/main.sh
-wget -O memos https://github.com/SinzMise/memos-on-replit/raw/main/memos.moe
+wget -O main.sh https://github.com/shangskr/memos-on-replit/raw/main/main.sh
+wget -O memos https://github.com/shangskr/memos-on-replit/raw/main/memos.moe
 cp -r build/.replit . && cp -r build/replit.nix .
 echo "恭喜搭建完成"
 echo "点击Run立即运行项目"
